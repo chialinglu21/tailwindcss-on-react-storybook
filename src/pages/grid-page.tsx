381662471b6b4c42cdb614f14c464191f35a1be2
ChiaLing <tobeciolynn@gmail.com>
2020-12-26 22:59:33 +0800

    update grid story

--- src/pages/grid-page.tsx
+++ src/pages/grid-page.tsx
@@ -1,14 +1,76 @@
 
-import React from 'react';
+import React, {FC} from 'react';
 import './grid-page.scss';
 
-export const GridPage = () => {
+const _initGaps: GridGap = {
+    defGap: 4,
+    smGap: 1.5,
+    mdGap: 4,
+    lgGap: 5,
+    xlGap: 10,
+    xxlGap: 10
+}
+
+export type GridGap = {
+    defGap: number;
+    smGap: number;
+    mdGap:number;
+    lgGap:number;
+    xlGap: number;
+    xxlGap: number;
+}
+
+export const GridPage: FC<GridGap> = (gaps: GridGap = _initGaps) => {
+    const DEFAULT_GAP = gaps?.defGap ? `gap-${gaps.defGap}`: '';
+    const RES_GAP = `sm:gap-${gaps.smGap} md:gap-${gaps.mdGap} lg:gap-${gaps.lgGap} xl:gap-${gaps.xlGap} xxl:gap-${gaps.xxlGap}`;
     return (
-        <div className="grid grid-cols-3 gap-4">
-            <div className="grid-box"></div>
-            <div className="grid-box"></div>
-            <div className="grid-box"></div>
-            <div className="grid-box"></div>
+        <div>
+            <p>grid grid-cols-1 {DEFAULT_GAP} {RES_GAP}</p>
+            <div className={`grid grid-cols-1 ${DEFAULT_GAP} ${RES_GAP} grid-container`}>
+                <div className="grid-box"></div>
+            </div>
+            
+            <p>grid grid-cols-2 {DEFAULT_GAP} {RES_GAP}</p>
+            <div className={`grid grid-cols-2 ${DEFAULT_GAP}  ${RES_GAP} grid-container`}>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+            </div>
+            
+            <p>grid grid-cols-3 {DEFAULT_GAP} {RES_GAP}</p>
+            <div className={`grid grid-cols-3 ${DEFAULT_GAP} ${RES_GAP} grid-container`}>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+            </div>
+            
+            <p>grid grid-cols-4 {DEFAULT_GAP} {RES_GAP}</p>
+            <div className={`grid grid-cols-4 ${DEFAULT_GAP} ${RES_GAP} grid-container`}>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+            </div>
+            
+            <p>grid grid-cols-5 {DEFAULT_GAP} {RES_GAP}</p>
+            <div className={`grid grid-cols-5 ${DEFAULT_GAP} ${RES_GAP} grid-container`}>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+            </div>
+            
+            <p>grid grid-cols-8 {DEFAULT_GAP} {RES_GAP}</p>
+            <div className={`grid grid-cols-8 ${DEFAULT_GAP} ${RES_GAP} grid-container`}>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+                <div className="grid-box"></div>
+            </div>
         </div>
     );
 };
--- src/stories/foundation/grid.stories.tsx
+++ src/stories/foundation/grid.stories.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
-import { GridPage } from '../../pages/grid-page';
+import { GridPage, GridGap} from '../../pages/grid-page';
+
+const _gapOptions=[0,0.5,1,1.5,2,2.5,3,3.5,4,5,6,7,8,9,10,11,12];
 
 export default{
     title: 'FOUNDATION/Grid',
-    component: GridPage 
+    component: GridPage,
+    argTypes: {
+        defGap: {
+            description: 'The space between elements',
+            control: {
+                type: 'select',
+                options: [..._gapOptions]
+            }
+        },
+        smGap: {
+            control: {
+                type: 'select',
+                options: [..._gapOptions]
+            }
+        },
+        mdGap: {
+            control: {
+                type: 'select',
+                options: [..._gapOptions]
+            }
+        },
+        xlGap: {
+            control: {
+                type: 'select',
+                options: [..._gapOptions]
+            }
+        },
+        xxlGap: {
+            control: {
+                type: 'select',
+                options: [..._gapOptions]
+            }
+        }
+    }
 } as Meta
 
-export const Grid  = () => <GridPage></GridPage>;
+export const Grid  = (args: GridGap) => <GridPage {...args}></GridPage>;
+Grid.args = {
+    defGap: 4,
+    smGap: 1.5,
+    mdGap: 4,
+    lgGap: 5,
+    xlGap: 10,
+    xxlGap: 10
+} as GridGap
